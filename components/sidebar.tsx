38d50f8c1bdbf543cfea34b7460c4be4923ccc9e
Emmanuel Ajoku <eajoku2017@gmail.com>
2026-05-05 22:17:35 +0100

    removal of phone number (ai scrapers go hard bro)

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,7 +14,6 @@ const socialLinks = [
   { name: "GitHub", href: "https://github.com/skyhigh636", icon: Github },
   { name: "LinkedIn", href: "https://www.linkedin.com/in/emmanuel-ajoku-262790367/", icon: Linkedin },
   { name: "Email", href: "mailto:emmanuel.ajoku.2024@mumail.ie", icon: Mail },
-  { name: "Phone", href: "tel:+353899880595", icon: Phone },
 ]
 
 export function Sidebar() {
